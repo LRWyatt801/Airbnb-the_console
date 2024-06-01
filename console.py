33247f492740ef00684cc5b4c19d0ddef9b4464f
DaveyCHaysIII <david.c.hays.iii@gmail.com>
2024-05-31 21:42:29 -0500

    added placeholder functions to flesh out tommorow

--- console.py
+++ console.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import cmd
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -21,8 +22,71 @@ class HBNBCommand(cmd.Cmd):
         return True
     
     def emptyline(self):
+        """this overrides the default 
+        behavior of hitting enter
+        pass is intentional"""
+        pass
+
+    ########### CRUD functions
+
+    def do_create():
+        """Create a new instance
+        of a class. Takes className
+        as argument"""
+        pass
+
+    def do_show():
+        """Prints string representation
+        of an instance. Takes arguments 
+        className and id"""
+        pass
+
+    def do_destroy():
+        """Deletes an instance based on
+        the className and id"""
+        pass
+
+    def do_all():
+        """Prints string representation of all instances, 
+        optional argument className"""
+        pass
+
+    def do_update():
+        """Updates an instance based on className and id
+        and attribute"""
         pass
     
+    ############Help Text
+
+    def help_EOF(self):
+        """help text for update"""
+        print("this text should override the doc!")
+
+    def help_quit(self):
+        """help text for quit"""
+        print("this text should override the doc!")
+
+    def help_create(self):
+        """help text for create"""
+        print("this text should override the doc!")
+
+    def help_show(self):
+        """help text for show"""
+        print("this text should override the doc!")
+
+    def help_destroy(self):
+        """help text for destroy"""
+        print("this text should override the doc!")
+
+    def help_all(self):
+        """help text for all"""
+        print("this text should override the doc!")
+
+    def help_update(self):
+        """help text for update"""
+        print("this text should override the doc!")
+
+
 if __name__ == '__main__':
     """This is the engine that makes the
     console work:
