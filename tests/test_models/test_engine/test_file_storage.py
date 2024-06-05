0f2f7a693506fabbe35a238ce72f2a0c7ba450c3
LRWyatt801 <loganwyatt1995@gmail.com>
2024-06-04 19:48:24 -0500

    New and save tests combined

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -7,6 +7,7 @@ import os
 from models.base_model import BaseModel
 from models.engine.file_storage import FileStorage
 from models import storage
+import json
 
 
 class TestFileStorage(unittest.TestCase):
@@ -31,15 +32,24 @@ class TestFileStorage(unittest.TestCase):
         expected_return = storage.all()
         self.assertIs(storage.all(), expected_return)
     
-    def test_new(self):
-        self.new_model = BaseModel()
-        self.assertIsNot(self.new_model, self.my_model)
+    # def test_new(self):
+    #     self.storage.new(self.my_model)
+    #     key = "{}.{}".format(self.my_model.__class__.__name__,self.my_model.id)
+    #     self.assertIn(key, self.storage)
 
-    def test_save(self):
-        self.new_model = BaseModel()
-        self.new_model.save()
-        self.new_data = self.storage.reload()
-        self.assertEqual(self.new_data, self.storage.reload())
+    # def test_save(self):
+    #     self.new_model = BaseModel()
+    #     self.new_model.save()
+    #     self.new_data = self.storage.reload()
+    #     self.assertEqual(self.new_data, self.storage.reload())
+
+    def test_new_and_save(self):
+        self.instance = BaseModel()
+        self.storage.new(self.instance)
+        self.storage.save()
+        with open('file.json', 'r') as file:
+            data = json.load(file)
+        self.assertIn("BaseModel.{}".format(self.instance.id), data)
     
     def test_reload(self):
         self.new_data = self.storage.reload()
